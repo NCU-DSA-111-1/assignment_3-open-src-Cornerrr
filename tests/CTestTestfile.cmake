e86d86a9d2d9177a967d9b21027c2ac4f06b2194
Cornertw <92680977+Cornerrr@users.noreply.github.com>
2023-01-10 20:24:52 +0800

    Delete tests directory

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Max Liu/NCU_Documents/arcd/tests
-# Build directory: /mnt/c/Users/Max Liu/NCU_Documents/arcd/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(codec_tests "/mnt/c/Users/Max Liu/NCU_Documents/arcd/tests/codec_tests")
-set_tests_properties(codec_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Max Liu/NCU_Documents/arcd/tests/CMakeLists.txt;10;add_test;/mnt/c/Users/Max Liu/NCU_Documents/arcd/tests/CMakeLists.txt;0;")
